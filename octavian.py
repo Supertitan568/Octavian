32c8777ef7113742afbea1344af377a00c4b3639
Supertitan568 <laodenbach@gmail.com>
2021-03-17 08:59:54 -0500

    Fixed some stuff. Added support for other characters besides regular alpha characters. Also made it so the input is a text file.

--- octavian.py
+++ octavian.py
@@ -1,16 +1,23 @@
-allLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ' ']
+allLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+def openfile (fileName):
+  file = open(fileName)
+  return file.read()
 def shiftLetters (shiftNumber):
-    i=0
-    shiftedLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ' ']
+    shiftedLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    i = 0
     while i < int(shiftNumber):
       shiftedLetters.append(shiftedLetters.pop(0))  
-      i=i+1
+      i += 1
     return shiftedLetters
 def encryptCypher (cypher,shiftedLetters):
+  print(cypher)
   x=''
   for lett in cypher:
-    place = allLetters.index(lett)
-    shiftplace = shiftedLetters[place]
-    x= x + shiftplace
+    if lett.isalpha() == True:
+      lett = lett.lower()
+      shiftplace = shiftedLetters[allLetters.index(lett)]
+      x += shiftplace
+    else:
+      x += lett
   return x
-print(encryptCypher(input('What message do you want to encypt?: '), shiftLetters(input('How much do you want this to be shifted by?: '))))
+print(encryptCypher(openfile(input('Name the file you want to encrypt: ')), shiftLetters(input('How much do you want this to be shifted by?: '))))
